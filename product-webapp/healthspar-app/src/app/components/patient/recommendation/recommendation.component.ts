09d4421624fc7d78e5e5f34955a8bfe27749618a
Roopesh Rokade <04012001roopesh@gmail.com>
2023-09-21 14:26:52 +0530

    Frontend part for Home(Recommendation)

--- product-webapp/healthspar-app/src/app/components/patient/recommendation/recommendation.component.ts
+++ product-webapp/healthspar-app/src/app/components/patient/recommendation/recommendation.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hospital } from 'src/app/model/hospital';
+import { RecommendationService } from 'src/app/service/recommendation.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-recommendation',
   templateUrl: './recommendation.component.html',
   styleUrls: ['./recommendation.component.css']
 })
-export class RecommendationComponent {
+export class RecommendationComponent implements OnInit{
+  recommendedHospitals: Hospital[] = [];
+
+  constructor(private recommendService:RecommendationService){}
+
+
+  ngOnInit(): void {
+    this.getRecommendations(6);
+  }
+
+  public getRecommendations(patientId:number):void{
+    this.recommendService.getRecommendedHospitals(patientId).subscribe(
+      (response:Hospital[])=>{
+        this.recommendedHospitals=response;
+        console.log(this.recommendedHospitals);
+      },
+      (error:HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    )
+    console.log(this.recommendedHospitals);
+    
+  }
+  
+  
 
 }
---
+++ product-webapp/healthspar-app/src/app/model/city.ts
@@ -0,0 +1,10 @@
+import { Hospital } from "./hospital";
+
+export interface City {
+    cityId: number;
+    name: string;
+    district: string;
+    state: string;
+    country: string;
+    zip: string;
+}
