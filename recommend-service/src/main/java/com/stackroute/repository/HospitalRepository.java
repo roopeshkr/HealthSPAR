15a690e61bdfb56e7581a07895b16d98324bcbb7
Roopesh Rokade <04012001roopesh@gmail.com>
2023-09-22 09:39:44 +0530

    Updating Backend part of recommendation service

--- recommend-service/src/main/java/com/stackroute/repository/HospitalRepository.java
+++ recommend-service/src/main/java/com/stackroute/repository/HospitalRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.neo4j.repository.query.Query;
 import java.util.List;
 
 public interface HospitalRepository extends Neo4jRepository<Hospital,Long> {
-    @Query("MATCH (h:Hospital)-[:LOCATED_IN]->(c:City) WHERE c.name = $cityName RETURN h")
-    List<Hospital> findByCity(String cityName);
+
 }
--- recommend-service/src/main/java/com/stackroute/service/RecommendationService.java
+++ recommend-service/src/main/java/com/stackroute/service/RecommendationService.java
@@ -6,25 +6,46 @@ import com.stackroute.model.Patient;
 import com.stackroute.repository.HospitalRepository;
 import com.stackroute.repository.PatientRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class RecommendationService {
     private final PatientRepository patientRepository;
     private final HospitalRepository hospitalRepository;
 
+//    public List<Hospital> recommendHospitalsForPatient(Long patientId)
+//    {
+//        Patient patient=patientRepository.findById(patientId).orElseThrow(
+//                ()->new PatientNotFoundException("Patient not found with id : "+patientId)
+//        );
+//        String patientCity= patient.getCity().getName();
+//        System.out.println("city : "+patientCity);
+//        List<Hospital> recommendedHospitals=hospitalRepository.findByCity(patientCity);
+//        log.info("query result : {}", recommendedHospitals);
+//        return recommendedHospitals;
+//    }
+
     public List<Hospital> recommendHospitalsForPatient(Long patientId)
     {
-        Patient patient=patientRepository.findById(patientId).orElseThrow(
-                ()->new PatientNotFoundException("Patient not found with id : "+patientId)
-        );
-        String patientCity= patient.getCity().getName();
-        System.out.println("Patient City: " + patientCity);
-        List<Hospital> recommendedHospitals=hospitalRepository.findByCity(patientCity);
-        System.out.println("Hospitals :"+recommendedHospitals);
-        return recommendedHospitals;
+        Patient patient = patientRepository.findById(patientId)
+                .orElseThrow(() -> new IllegalArgumentException("Patient not found"));
+
+        String patientCity = patient.getCity().getName();
+
+        List<Hospital> allHospitals = hospitalRepository.findAll();
+
+        return allHospitals.stream()
+                .filter(hospital -> hospital.getCity().getName().equals(patientCity))
+                .collect(Collectors.toList());
     }
+
+
+
+
 }
