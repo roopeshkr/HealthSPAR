1e7083734482e347343a3f268d97d7ff495596f2
Roopesh Rokade <04012001roopesh@gmail.com>
2023-09-24 21:21:40 +0530

    backend part of patient service

---
+++ product-webapp/healthspar-app/src/app/model/patient.ts
@@ -0,0 +1,14 @@
+export interface Patient {
+  patientId: string;
+  patientName: string;
+  email: string;
+  phoneNumber: string;
+  dob: Date;
+  bloodGroup: string;
+  gender: number;
+  cityName: string;
+  district: string; 
+  state: string;
+  country: string;
+  zip: string;
+}
