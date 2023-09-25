cc1cad9e6ab6edab4c44470954a77c9c48857786
Roopesh Rokade <04012001roopesh@gmail.com>
2023-09-25 23:08:33 +0530

    Profile Form integration with Backend

--- product-webapp/healthspar-app/src/app/components/hospital/hospital-details/hospital-details.component.ts
+++ product-webapp/healthspar-app/src/app/components/hospital/hospital-details/hospital-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Hospital } from 'src/app/model/hospital';
+import { HospitalService } from 'src/app/service/hospital.service';
 @Component({
   selector: 'app-hospital-details',
   templateUrl: './hospital-details.component.html',
@@ -11,6 +14,48 @@ export class HospitalDetailsComponent {
     experience: string;
     language: string;
   }> = [];
+
+  hospitalProfileForm: FormGroup;
+  isSubmitted: boolean = false;
+
+  constructor(private profileService: HospitalService, private formBuilder: FormBuilder) {
+    this.hospitalProfileForm = this.formBuilder.group(
+      {
+        hospitalName: ['',Validators.required],
+        hospitalWebsite: [''],
+        hospitalEmail: ['',Validators.email],
+        hospitalPhoneNumber: ['',Validators.pattern('^[0-9]*$'),Validators.maxLength(10),Validators.minLength(10)],
+        hospitalImageURL: '',
+        hospitalRating: [0,Validators.max(5),Validators.min(0)],
+        hospitalReviews: [''],
+        city: {
+          cityId: 0,
+          name: ['',Validators.required],
+          district: ['',Validators.required],
+          state: ['',Validators.required],
+          country: ['',Validators.required],
+          zip: ['',Validators.pattern('^[0-9]*$'),Validators.minLength(6),Validators.maxLength(6)],
+        },
+        hospitalAmenities: [],
+        numberOfBeds: [0,Validators.min(1)],
+        doctors: [],
+        specialty: [],
+      }
+    )
+  }
+
+  onSubmit(){
+    this.isSubmitted=true;
+    if(this.hospitalProfileForm.valid){
+      const HospitalData:Hospital=this.hospitalProfileForm.value;
+      this.profileService.addHospitalProfile(HospitalData).subscribe(
+        (response)=>{
+          console.log("Hospital Added successfully:",response);
+          
+        }
+      )
+    }
+  }
 
   removeDoctor(index: number) {
     this.doctors.splice(index, 1);
