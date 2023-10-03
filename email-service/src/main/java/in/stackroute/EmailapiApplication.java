ea250916d0587bbaa916b28bd10c4fa9c57e5e9a
sumandas201820 <sumandas201820@gmail.com>
2023-10-03 11:43:38 +0530

    email service

---
+++ email-service/src/main/java/in/stackroute/EmailapiApplication.java
@@ -0,0 +1,12 @@
+package in.stackroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailapiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmailapiApplication.class, args);
+
+    }
+}
