59d053c48976078173130c22d465a65e49616d07
04012001roopesh <04012001roopesh@gmail.com>
2023-10-09 12:59:53 +0530

    API Gateway Integration

--- product-webapp/healthspar-app/src/app/components/patient/hospital/hospital.component.ts
+++ product-webapp/healthspar-app/src/app/components/patient/hospital/hospital.component.ts
@@ -11,7 +11,6 @@ import { HospitalService } from 'src/app/service/hospital.service';
   styleUrls: ['./hospital.component.css'],
 })
 export class HospitalComponent implements OnInit {
-<<<<<<< HEAD
   allHospitals: Hospital[] = [];
   recommendedHospitals: Hospital[] = [];
   selectedCity: string = 'Select City';
@@ -20,16 +19,6 @@ export class HospitalComponent implements OnInit {
   constructor(
     private hospitalService: HospitalService,
     private recommendService: RecommendationService,
-=======
-  allHospitals:Hospital[]=[];
-  recommendedHospitals: Hospital[] = [];
-  selectedCity:string="Select City";
-  cityOptions:Set<string>=new Set();
-
-  constructor(
-    private hospitalService: HospitalService,
-    private recommendService:RecommendationService,
->>>>>>> bf5e039a07c07c68b6e8e5395df53b11f79923f7
     private route: Router,
     private router: ActivatedRoute
   ) {}
@@ -38,7 +27,6 @@ export class HospitalComponent implements OnInit {
     this.getAllHospitals();
   }
 
-<<<<<<< HEAD
   public getAllHospitals(): void {
     this.hospitalService.getAllHospitals().subscribe((response: Hospital[]) => {
       this.allHospitals = response;
@@ -51,31 +39,10 @@ export class HospitalComponent implements OnInit {
 
       console.log('city options : ', this.cityOptions);
     });
-=======
-  public getAllHospitals():void{
-    this.hospitalService.getAllHospitals().subscribe(
-      (response:Hospital[])=>{
-        this.allHospitals=response;
-        console.log("All Hospitals : ",response);
-        
-        
-        this.allHospitals.forEach(
-          (hospital)=>{
-            this.cityOptions.add('Select City');
-            this.cityOptions.add(hospital.city.name.toLowerCase())
-          }
-          )
-
-        console.log("city options : ",this.cityOptions);
-        
-      }
-    )
->>>>>>> bf5e039a07c07c68b6e8e5395df53b11f79923f7
   }
 
   public onCitySelect(event: Event): void {
     const selectedValue = (event.target as HTMLSelectElement).value;
-<<<<<<< HEAD
     this.selectedCity = selectedValue;
     if (selectedValue === 'Select City') {
       this.recommendedHospitals = this.allHospitals;
@@ -95,29 +62,7 @@ export class HospitalComponent implements OnInit {
   onBookClick(hospitalId: number): void {
     this.route.navigate(['/patient/hospital-page', hospitalId]);
   }
-=======
-    this.selectedCity = selectedValue; 
-    if (selectedValue === 'Select City') {
-      this.recommendedHospitals = this.allHospitals;
-      console.log("not selected : ",this.recommendedHospitals);
-      
-    } else {
-      this.getRecommendations(selectedValue);
-    }
->>>>>>> bf5e039a07c07c68b6e8e5395df53b11f79923f7
 }
 
 
-  public getRecommendations(cityName: string): void {
-    this.recommendService.getRecommendedHospitals(cityName).subscribe(
-      (response: Hospital[]) => {
-        this.recommendedHospitals = response;
-      }
-    )
-
-  }
-  onBookClick(hospitalId: number): void {
-    this.route.navigate(['/patient/hospital-page', hospitalId]);
-  }
-
-}
+  
