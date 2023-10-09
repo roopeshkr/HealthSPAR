90fb66f287178140a4ccb911f88565e5b2621858
04012001roopesh <04012001roopesh@gmail.com>
2023-10-09 22:38:21 +0530

    dockerization and APi gateway

--- product-webapp/healthspar-app/src/app/service/recommendation.service.ts
+++ product-webapp/healthspar-app/src/app/service/recommendation.service.ts
@@ -8,11 +8,11 @@ import { Hospital } from '../model/hospital';
 })
 
 export class RecommendationService {
-    private apiUrl = 'http://localhost:8086/api/v1/hospital/patient';
+    private apiUrl = 'http://localhost:8086';
 
     constructor(private http: HttpClient) { }
     
     public getRecommendedHospitals(cityName:string): Observable<Hospital[]> {
-        return this.http.get<Hospital[]>(`${this.apiUrl}/${cityName}`);
+        return this.http.get<Hospital[]>(`${this.apiUrl}/api/v1/hospital/patient/${cityName}`);
     }
 }
